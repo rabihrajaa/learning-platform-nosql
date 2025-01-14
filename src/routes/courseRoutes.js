842027b0e1cd44992e1d600c92d3f323d5812cda
rabihrajaa <rajaarabihh@gmail.com>
2025-01-14 18:42:09 +0100

    addRoutes

--- src/routes/courseRoutes.js
+++ src/routes/courseRoutes.js
@@ -1,8 +1,3 @@
-// Question: Pourquoi séparer les routes dans différents fichiers ?
-// Réponse : 
-// Question : Comment organiser les routes de manière cohérente ?
-// Réponse: 
-
 const express = require('express');
 const router = express.Router();
 const courseController = require('../controllers/courseController');
